d4bf80cd8e85359362c5b7919822ba0576e4fe63
VladimirGorin <vladimirgorin410@gmail.com>
2024-12-08 23:57:19 +0600

    fix error with typeorm in prod

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,4 +1,5 @@
 import { DataSource } from "typeorm";
+import path from "path";
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -9,9 +10,9 @@ const AppDataSource = new DataSource({
   database: process.env.POSTGRES_DB,
   synchronize: true, // True only for dev!
   logging: false,
-  entities: ["src/database/models/**/*.ts"],
-  migrations: ["src/database/migrations/**/*.ts"],
-  subscribers: ["src/database/subscribers/**/*.ts"],
+  entities: [path.join(__dirname, "models/**/*.js")],
+  migrations: [path.join(__dirname, "migrations/**/*.js")],
+  subscribers: [path.join(__dirname, "subscribers/**/*.js")],
 });
 
 export default AppDataSource;
