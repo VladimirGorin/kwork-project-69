f8486ed0b6065f17f8f0a04ac7cf08a1311a1923
VladimirGorin <vladimirgorin410@gmail.com>
2024-12-09 00:19:45 +0600

    fix error with typeorm in prod

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,35 +1,19 @@
 import { DataSource } from "typeorm";
-import path from "path";
-
-const isDevelopment = process.env.NODE_ENV === "development";
+import { User } from "./database/models/User";
+import { Message } from "./database/models/Message";
 
 const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: isDevelopment ? true : false, // True only for dev!
-    logging: false,
-    entities: [
-        path.join(
-            __dirname,
-            isDevelopment ? "models/**/*.ts" : "models/**/*.js"
-        ),
-    ],
-    migrations: [
-        path.join(
-            __dirname,
-            isDevelopment ? "migrations/**/*.ts" : "migrations/**/*.js"
-        ),
-    ],
-    subscribers: [
-        path.join(
-            __dirname,
-            isDevelopment ? "subscribers/**/*.ts" : "subscribers/**/*.js"
-        ),
-    ],
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true, // True only for dev!
+  logging: true,
+  entities: [User, Message],
+  migrations: [],
+  subscribers: [],
 });
 
 export default AppDataSource;
