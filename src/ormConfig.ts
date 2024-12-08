de9e2e6c72a98b66547b8845c6c33ddc53a53e9c
VladimirGorin <vladimirgorin410@gmail.com>
2024-12-09 00:00:04 +0600

    fix error with typeorm in prod

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,18 +1,35 @@
 import { DataSource } from "typeorm";
 import path from "path";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: true, // True only for dev!
-  logging: false,
-  entities: [path.join(__dirname, "models/**/*.js")],
-  migrations: [path.join(__dirname, "migrations/**/*.js")],
-  subscribers: [path.join(__dirname, "subscribers/**/*.js")],
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: isDevelopment ? true : false, // True only for dev!
+    logging: false,
+    entities: [
+        path.join(
+            __dirname,
+            isDevelopment ? "models/**/*.ts" : "models/**/*.js"
+        ),
+    ],
+    migrations: [
+        path.join(
+            __dirname,
+            isDevelopment ? "migrations/**/*.ts" : "migrations/**/*.js"
+        ),
+    ],
+    subscribers: [
+        path.join(
+            __dirname,
+            isDevelopment ? "subscribers/**/*.ts" : "subscribers/**/*.js"
+        ),
+    ],
 });
 
 export default AppDataSource;
